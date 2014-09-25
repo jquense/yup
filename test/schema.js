99d9328a928310f14890b9ebfbc498c720c1e9c4
theporchrat <theporchrat@gmail.com>
2014-09-25 17:39:56 -0400

    start on resource

--- test/schema.js
+++ test/schema.js
@@ -11,6 +11,7 @@ chai.should();
 
 describe( 'Schema', function(){
 
+
   it( 'should create fields correctly', function(){
     var schema = new Schema({
       fields: {
@@ -24,13 +25,13 @@ describe( 'Schema', function(){
       }
     })
 
-    var fields = schema.fields()
+    var fields = schema.fields
 
     fields.should.have.property('number').that.is.an.instanceOf(ft.Number)
     fields.should.have.property('string').that.is.an.instanceOf(ft.String)
 
     fields.should.have.property('nested').that.is.an.instanceOf(Schema)
-      .that.has.deep.property('_fields.string')
+      .that.has.deep.property('fields.string')
       .that.is.an.instanceOf(ft.String)
 
     fields.should.have.property('arrayOfString').that.is.an.instanceOf(ft.Array)
@@ -40,6 +41,21 @@ describe( 'Schema', function(){
       .and.has.deep.property('validations.length').that.equals(1)
   })
 
+  it( 'should inherit fields', function(){
+    var SchemaA = Schema.extend({
+      fields: { number: new ft.Number }
+    })
+
+    var SchemaB = SchemaA.extend(
+      { fields: { string: new ft.String }},
+      { fields: { otherString: new ft.String} })
+
+    var fields = (new SchemaB).fields
+
+    fields.should.have.property('number').that.is.an.instanceOf(ft.Number)
+    fields.should.have.property('string').that.is.an.instanceOf(ft.String)
+    fields.should.have.property('otherString').that.is.an.instanceOf(ft.String)
+  })
 
   it( 'should validate correctly', function(){
     var schema = new Schema({
