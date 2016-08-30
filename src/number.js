6fe2941cb8e074fb21d586ce05a61abf658b53b4
Александр <toca@inbox.ru>
2016-08-30 23:13:08 +0300

    [added] number methods less, more, notEqual

--- src/number.js
+++ src/number.js
@@ -9,86 +9,123 @@ let isNaN = value => value != +value
 let isInteger = val => isAbsent(val) || val === (val | 0)
 
 export default function NumberSchema() {
-  if ( !(this instanceof NumberSchema))
-    return new NumberSchema()
+	if ( !(this instanceof NumberSchema))
+		return new NumberSchema()
 
-  MixedSchema.call(this, { type: 'number' })
+	MixedSchema.call(this, { type: 'number' })
 
-  this.withMutation(() => {
-    this.transform(function(value) {
-      if (this.isType(value)) return value
+	this.withMutation(() => {
+		this.transform(function(value) {
+			if (this.isType(value)) return value
 
-      let parsed = parseFloat(value);
-      if (this.isType(parsed)) return parsed
+			let parsed = parseFloat(value);
+			if (this.isType(parsed)) return parsed
 
-      return NaN;
-    })
-  })
+			return NaN;
+		})
+	})
 }
 
 inherits(NumberSchema, MixedSchema, {
 
-  _typeCheck(value) {
-    if (value instanceof Number)
-      value = value.valueOf();
+	_typeCheck(value) {
+		if (value instanceof Number)
+			value = value.valueOf();
 
-    return typeof value === 'number' && !isNaN(value)
-  },
+		return typeof value === 'number' && !isNaN(value)
+	},
 
-  min(min, msg) {
-    return this.test({
-      name: 'min',
-      exclusive: true,
-      params: { min },
-      message: msg || locale.min,
-      test(value) {
-        return isAbsent(value) || value >= this.resolve(min)
-      }
-    })
-  },
+	min(min, msg) {
+		return this.test({
+			name: 'min',
+			exclusive: true,
+			params: { min },
+			message: msg || locale.min,
+			test(value) {
+				return isAbsent(value) || value >= this.resolve(min)
+			}
+		})
+	},
 
-  max(max, msg) {
-    return this.test({
-      name: 'max',
-      exclusive: true,
-      params: { max },
-      message: msg || locale.max,
-      test(value) {
-        return isAbsent(value) || value <= this.resolve(max)
-      }
-    })
-  },
+	max(max, msg) {
+		return this.test({
+			name: 'max',
+			exclusive: true,
+			params: { max },
+			message: msg || locale.max,
+			test(value) {
+				return isAbsent(value) || value <= this.resolve(max)
+			}
+		})
+	},
 
-  positive(msg) {
-    return this.min(0, msg || locale.positive)
-  },
+	less(less, msg) {
+		return this.test({
+			name: 'less',
+			exclusive: true,
+			params: { less },
+			message: msg || locale.less,
+			test(value) {
+				return isAbsent(value) || value < this.resolve(less)
+			}
+		})
+	},
 
-  negative(msg) {
-    return this.max(0, msg || locale.negative)
-  },
 
-  integer(msg) {
-    msg = msg || locale.integer;
+	more(more, msg) {
+		return this.test({
+			name: 'more',
+			exclusive: true,
+			params: { more },
+			message: msg || locale.more,
+			test(value) {
+				return isAbsent(value) || value > this.resolve(more)
+			}
+		})
+	},
 
-    return this.test('integer', msg, isInteger)
-  },
+	notEqual(notEqual, msg) {
+		return this.test({
+			name: 'notEqual',
+			exclusive: true,
+			params: { notEqual },
+			message: msg || locale.notEqual,
+			test(value) {
+				return isAbsent(value) || value !== this.resolve(notEqual)
+			}
+		})
+	},
 
-  truncate() {
-    return this.transform(value =>
-      !isAbsent(value) ? (value | 0) : value)
-  },
+	positive(msg) {
+		return this.min(0, msg || locale.positive)
+	},
 
-  round(method) {
-    var avail = ['ceil', 'floor', 'round', 'trunc']
-    method = (method && method.toLowerCase()) || 'round'
+	negative(msg) {
+		return this.max(0, msg || locale.negative)
+	},
 
-    // this exists for symemtry with the new Math.trunc
-    if (method === 'trunc')
-      return this.truncate()
+	integer(msg) {
+		msg = msg || locale.integer;
 
-    if (avail.indexOf(method.toLowerCase()) === -1)
-      throw new TypeError('Only valid options for round() are: ' + avail.join(', '))
+		return this.test('integer', msg, isInteger)
+	},
 
-    return this.transform(value => !isAbsent(value) ? Math[method](value) : value)
-  }
+	truncate() {
+		return this.transform(value =>
+			!isAbsent(value) ? (value | 0) : value)
+	},
+
+	round(method) {
+		var avail = ['ceil', 'floor', 'round', 'trunc']
+		method = (method && method.toLowerCase()) || 'round'
+
+		// this exists for symemtry with the new Math.trunc
+		if (method === 'trunc')
+			return this.truncate()
+
+		if (avail.indexOf(method.toLowerCase()) === -1)
+			throw new TypeError('Only valid options for round() are: ' + avail.join(', '))
+
+		return this.transform(value => !isAbsent(value) ? Math[method](value) : value)
+	}
 })
