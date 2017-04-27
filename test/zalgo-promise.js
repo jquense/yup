29497af73e6b8944ac42d7237561785ba83b2769
Jason Quense <monastic.panic@gmail.com>
2017-04-27 12:48:32 -0400

    WIP

---
+++ test/zalgo-promise.js
@@ -0,0 +1,16 @@
+// import Promise from 'promise/src/es6-extensions';
+
+// import ZalgoPromise from '../src/util/ZalgoPromise';
+
+// require('promises-aplus-tests')({
+//     resolved: (v) => ZalgoPromise.resolve(v),
+//     rejected: (reason) => ZalgoPromise.rejec(reason),
+//     deferred: () => {
+//       let d = {};
+//       d.promise = new ZalgoPromise((y, n) => {
+//         d.resolve = y
+//         d.reject = n
+//       })
+//     }
+//   });
+// });
