0002a3dd74b583c8eba6f3c6e7d3c0edb35708c8
Jason Quense <monastic.panic@gmail.com>
2017-05-23 10:50:39 -0400

    fix tests

--- test-setup.js
+++ test-setup.js
@@ -1,15 +1,22 @@
-require('babel-polyfill')
+// require('babel-polyfill')
+
 global.chai = require('chai')
 global.sinon = require('sinon')
-
 
 global.chai.use(require('sinon-chai'))
 global.chai.use(require('chai-as-promised'))
 global.chai.use(require('dirty-chai'))
 
+global.expect = global.chai.expect;
 global.chai.should();
 
-global.expect = window.expect = global.chai.expect;
-global.TestHelpers = window.TestHelpers = require('./test/helpers');
+// WTF???
+Object.defineProperty(
+  Promise.prototype,
+  'should',
+  Object.getOwnPropertyDescriptor(Object.prototype, 'should')
+);
+
+global.TestHelpers = require('./test/helpers');
 
 global.specify = global.it
