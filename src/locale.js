9b4193821ff24b4e798e3a3ebfa954d1ee9cf2f3
Gabriel R. Abreu <grsabreu@gmail.com>
2017-07-18 16:47:52 -0300

    Add custom locale dictionary support
    Supports now the user passing her own locale which will get deep merged and the missing fields will default to the current one

--- src/locale.js
+++ src/locale.js
@@ -1,13 +1,19 @@
+import { merge } from 'lodash/fp'
+import { getLocale } from './customLocale'
 
-export let mixed = {
+const customLocale = getLocale()
+
+console.log('customLocale', customLocale)
+
+export let mixed = merge(customLocale.mixed, {
   default:   '${path} is invalid',
   notType:   '${path} must be a `${type}` type, got: "${value}" instead',
   required:  '${path} is a required field',
   oneOf:     '${path} must be one the following values: ${values}',
   notOneOf:  '${path} must not be one the following values: ${values}'
-}
+})
 
-export let string = {
+export let string = merge({
   required:  '${path} is a required field',
   length:    '${path} must be exactly ${length} characters',
   min:       '${path} must be at least ${min} characters',
@@ -18,32 +24,32 @@ export let string = {
   trim:      '${path} must be a trimmed string',
   lowercase: '${path} must be a lowercase string',
   uppercase: '${path} must be a upper case string'
-}
+}, customLocale.string)
 
-export let number = {
+export let number = merge(customLocale.number, {
   min:       '${path} must be greater than or equal to ${min}',
   max:       '${path} must be less than or equal to ${max}',
   positive:  '${path} must be a positive number',
   negative:  '${path} must be a negative number',
   integer:   '${path} must be an integer',
-};
+});
 
-export let date = {
+export let date = merge(customLocale.date, {
   min:       '${path} field must be later than ${min}',
   max:       '${path} field must be at earlier than ${max}',
-}
+})
 
 export let boolean = {};
 
-export let object = {
+export let object = merge(customLocale.object, {
   noUnknown: '${path} field cannot have keys not specified in the object shape',
-}
+})
 
-export let array = {
+export let array = merge(customLocale.array, {
   required:  '${path} is a required field',
   min:       '${path} field must have at least ${min} items',
   max:       '${path} field must have less than ${max} items',
-}
+})
 
 export default {
   mixed,
