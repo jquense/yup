aaf1363ba625c896ca0ceaaa2d94547e141a92b3
Haydn <haydnhkim@gmail.com>
2017-09-20 22:21:13 +0900

    Support for setLocale regardless of import order

--- test/customLocale.js
+++ test/customLocale.js
@@ -1,7 +1,13 @@
-import { setLocale, getLocale } from '../src/customLocale'
+import { setLocale } from '../src/customLocale'
 
 describe('Custom locale', () => {
+  it('should get default locale', () => {
+    const locale = require('../src/locale').default
+    expect(locale.string.email).to.equal('${path} must be a valid email')
+  })
+
   it('should set a new locale', () => {
+    const locale = require('../src/locale').default
     const dict = {
       string: {
         email: 'Invalid email',
@@ -10,11 +16,11 @@ describe('Custom locale', () => {
 
     setLocale(dict)
 
-    expect(getLocale()).to.deep.equal(dict)
+    expect(locale.string.email).to.equal(dict.string.email)
   })
 
   it('should update the main locale', () => {
     const locale = require('../src/locale').default
-    expect(locale.string).to.deep.include(getLocale().string)
+    expect(locale.string.email).to.equal('Invalid email')
   })
 })
