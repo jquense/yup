2a8a5391a409c38763fc93f3de1aa6055dec721e
Karl Horky <karl.horky@gmail.com>
2024-07-02 12:43:01 +0200

    Improve clarity

--- src/schema.ts
+++ src/schema.ts
@@ -719,7 +719,7 @@ export default abstract class Schema<
     if (opts.message === undefined) opts.message = locale.default;
 
     if (typeof opts.test !== 'function')
-      throw new TypeError('The `test` function parameter is a required parameter');
+      throw new TypeError('The `test` function parameter is required');
 
     let next = this.clone();
     let validate = createValidation(opts);
